1b4d4a4941137a7805490cb10145e6581b8b3071
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-06-01 14:09:21 +0200

    Ajout de la classe Site pour gérer les URLs et les titres, et amélioration des fonctions getTitle et getValidURLs pour utiliser des listes et des ensembles.

--- src/HTMLParser.py
+++ src/HTMLParser.py
@@ -1,12 +1,33 @@
-import re
+from __future__ import annotations
+from re import *
+
+class Site:
+	def __init__(self, smallUrl: str, title: str, parent: Site):
+		self.url: str = smallUrl
+		self.title: str = title
+		self.parents: set[Site] = set()
+
+		if parent != None and type(parent) == Site:
+			self.parents.add(parent)
+		else:
+			self.parents = None
+
+	def __eq__(self, value) -> bool:
+		if (type(value) != Site):
+			raise ValueError("Can't compare Site to non-Site objects")
+		else:
+			return self.url == value.url
 
 def getTitle(html: str) -> str:
-	correspondance: str = re.search(r'<title[^>]*>(.*?)</title>', html, re.IGNORECASE | re.DOTALL)
+	correspondance: list[str] = findall(r'<title[^>]*>(.*?)</title>', html, IGNORECASE | DOTALL)
 
-	
+	if len(correspondance) == 0:
+		return ""
+	else:
+		return correspondance[0]
 
 def getValidURLs(html: str) -> set[str]:
-	hrefs = set(re.findall(r'<a[^>]+href=["\']?([^"\'>\s]+)', html, re.IGNORECASE))
+	hrefs = set(findall(r'<a[^>]+href=["\']?([^"\'>\s]+)', html, IGNORECASE))
 	invalidHrefs = set()
 
 	for href in hrefs:
